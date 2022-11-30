e8a9f82a3703baa4e3bbf0c965f7b706d2208b6b
Yash Kumar <yashkc2025@gmail.com>
2022-11-30 14:53:04 +0530

    added secret key

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'ed4827fa24888390c248e8100c02654c48a3d22d6202981d8d520d909559c3fd5c6703b3049ffcb6a5eec601fb1225ecc1547087b21456bd047bc4d2f8ce5a24985e91e1ad3778814b91e1c47bdd13b9f3123383f863aeb0515172315ed8f0275238f0'
 
 posts = [
     {
@@ -24,6 +28,9 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
